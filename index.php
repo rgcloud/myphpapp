b8782c059ce5e17b3d5205e365d9e66bc17dcafb
rgcloud <37324173+rgcloud@users.noreply.github.com>
2018-03-17 19:28:38 +0530

    new file

--- index.php
+++ index.php
@@ -2,11 +2,11 @@
 <html>
 <body>
 
-<h1>This is my PHP Test Application</h1>
+<center> <h1> Welcome To our openshift DevOps(CI/CD) project</h1> </center>
 
 <?php
 
-echo "Version: 5.0";
+echo "Hi Everyone :-)";
 
 ?>
 
